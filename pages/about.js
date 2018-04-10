d105fa0ebdf2bec68b6756325826ccf72500cdfa
Mikhail Metrikin <metrikin@gmail.com>
2018-04-09 21:57:24 -0700

    SAve progress

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
-import Layout from '../comps/MyLayout'
+import Layout from "../comps/MyLayout";
 
 const About = () => (
   <Layout>
     <p>This is an about page</p>
   </Layout>
-)
+);
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,65 @@
-import Layout from '../comps/MyLayout'
-import Link from 'next/link'
+import Layout from "../components/MyLayout.js";
+import Link from "next/link";
 
-const PostLink = (props) => (
+function getPosts() {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
+  ];
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
     </Link>
-  </li>
-)
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
 
-// const Index = () => (
-//   <Layout>
-//     <p>Hello next.js</p>
-//   </Layout>
-// )
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
 
 export default () => (
   <Layout>
     <h1>My Blog</h1>
-    <ul>
-      <PostLink title="Hello Next.js"/>
-      <PostLink title="Learn Next.js is awesome"/>
-      <PostLink title="Deploy apps with Zeit"/>
-    </ul>
+    <ul>{getPosts().map(post => <PostLink key={post.id} post={post} />)}</ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
-)
+);
